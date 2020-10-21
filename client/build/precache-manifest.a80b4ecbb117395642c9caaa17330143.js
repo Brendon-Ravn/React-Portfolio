94162526e97a07183383fcb362313cac9f958ec7
Brendon Conatser <b.conatser21@gmail.com>
2020-10-20 20:27:39 -0500

    fixed build

---
+++ client/build/precache-manifest.a80b4ecbb117395642c9caaa17330143.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "da7d2a35165181df27ed",
+    "url": "/static/css/main.bba73411.chunk.css"
+  },
+  {
+    "revision": "da7d2a35165181df27ed",
+    "url": "/static/js/main.30b0627f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7b67b1cc43f329b282d7",
+    "url": "/static/css/2.e077c917.chunk.css"
+  },
+  {
+    "revision": "7b67b1cc43f329b282d7",
+    "url": "/static/js/2.bfbabdcf.chunk.js"
+  },
+  {
+    "revision": "d5fbff87724a0e334b5f39c80f4ae0fb",
+    "url": "/static/media/Brendon1.d5fbff87.png"
+  },
+  {
+    "revision": "4ee058917af64763fe076d1130c55f9f",
+    "url": "/static/media/BurgerApp.4ee05891.png"
+  },
+  {
+    "revision": "79e43bd51a0ae25c741bd72125a0a625",
+    "url": "/static/media/CodeQ_14.79e43bd5.png"
+  },
+  {
+    "revision": "5e47eb6d9e1b9eebed6a6e3c37f03905",
+    "url": "/static/media/Coding-Quiz.5e47eb6d.png"
+  },
+  {
+    "revision": "e819289764083622ea3766f782a15d1a",
+    "url": "/static/media/Dybs.e8192897.png"
+  },
+  {
+    "revision": "54d7d2536c27f8ab022291c0aed22dfd",
+    "url": "/static/media/Weather-Dashboard.54d7d253.png"
+  },
+  {
+    "revision": "57be1675bee0b657165cac61f9a0ec4b",
+    "url": "/static/media/Workday-Scheduler.57be1675.png"
+  },
+  {
+    "revision": "444cea35534af4a9609c903b97c2712f",
+    "url": "/static/media/double-bubble-dark.444cea35.png"
+  },
+  {
+    "revision": "4faa272ca742bee9fa009ab553044acf",
+    "url": "/index.html"
+  }
+];
