f83085c2b5bd198da44d12e2415fcc80540c96d4
Brendon Conatser <b.conatser21@gmail.com>
2023-03-14 16:18:52 -0500

    changed wording

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -18,7 +18,7 @@ function About() {
           <div className="col-lg-7 aboutMe">
             <p>
               Hi, my name is Brendon Conatser. I live in Murfreesboro, TN with my wife Brittany and 2 children, Isaac and
-              Audrey. I love gaming, music, fishing, and just hanging out with my family. I recently graduated from 
+              Audrey. I love gaming, music, fishing, and just hanging out with my family. I am a graduate of 
               Vanderbilt's Full Stack Web Development Bootcamp.
           </p>
             <br />
